a7e16c66a8c614fdada33ab57fc8b70e6f42c2fd
rutu-sh <ruturajs10@hotmail.com>
2025-07-22 16:07:25 -0400

    feat: new design using tailwind

---
+++ scripts/flip.js
@@ -0,0 +1,10 @@
+function flipCard() {
+  const card = document.getElementById('flip-card');
+  card.classList.toggle('rotate-y-180');
+}
+
+
+  function flipSyncronCard() {
+    const card = document.getElementById("syncron-card");
+    card.classList.toggle("rotate-y-180");
+  }
